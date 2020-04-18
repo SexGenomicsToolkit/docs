b48fb59de8e09dccbc68aa01cc8b3e81531345b9
RomainFeron <romainferon91@gmail.com>
2020-04-18 11:18:49 +0200

    Added .nojekyll file that should make the theme visible

--- conf.py
+++ conf.py
@@ -16,7 +16,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -76,12 +77,16 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': False,
+    'navigation_depth': 2,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
